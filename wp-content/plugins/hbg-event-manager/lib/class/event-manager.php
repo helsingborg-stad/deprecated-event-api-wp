b0962de7c0739630044225540bc810319617c843
Sebastian Thulin <sebastian.thulin@gmail.com>
2015-10-24 16:21:29 +0200

    Cpts location and organisation

--- wp-content/plugins/hbg-event-manager/lib/class/event-manager.php
+++ wp-content/plugins/hbg-event-manager/lib/class/event-manager.php
@@ -10,10 +10,12 @@
 	Class HbgEventManager {
 		
 		public function __construct() {
-			 add_action('init', '\Helsingborg\EventManager\HbgEventManager::register_cpt');
+			add_action('init', '\Helsingborg\EventManager\HbgEventManager::register_cpt_event');
+			add_action('init', '\Helsingborg\EventManager\HbgEventManager::register_cpt_location');
+			add_action('init', '\Helsingborg\EventManager\HbgEventManager::register_cpt_organisation');
 		}
 	
-		public static function register_cpt () {
+		public static function register_cpt_event () {
 		
 			$labels = array(
 				'name'               => _x( 'Event', 'post type general name', 'hbg-event-manager' ),
@@ -44,11 +46,89 @@
 				'capability_type'    => 'post',
 				'has_archive'        => true,
 				'hierarchical'       => false,
-				'menu_position'      => 10,
-				'supports'           => array( 'title', 'editor', 'author' )
+				'menu_position'      => 11,
+				'supports'           => array( 'title', 'editor' )
 			);
 		
 			register_post_type( 'event', $args );	
+			
+		}
+		
+		public static function register_cpt_location () {
+		
+			$labels = array(
+				'name'               => _x( 'Location', 'post type general name', 'hbg-event-manager' ),
+				'singular_name'      => _x( 'Location', 'post type singular name', 'hbg-event-manager' ),
+				'menu_name'          => _x( 'Locations', 'admin menu', 'hbg-event-manager' ),
+				'name_admin_bar'     => _x( 'Locations', 'add new on admin bar', 'hbg-event-manager' ),
+				'add_new'            => _x( 'Add New', 'book', 'hbg-event-manager' ),
+				'add_new_item'       => __( 'Add New Location', 'hbg-event-manager' ),
+				'new_item'           => __( 'New Location', 'hbg-event-manager' ),
+				'edit_item'          => __( 'Edit Location', 'hbg-event-manager' ),
+				'view_item'          => __( 'View Location', 'hbg-event-manager' ),
+				'all_items'          => __( 'All Location', 'hbg-event-manager' ),
+				'search_items'       => __( 'Search Locations', 'hbg-event-manager' ),
+				'parent_item_colon'  => __( 'Parent Locations:', 'hbg-event-manager' ),
+				'not_found'          => __( 'No location found.', 'hbg-event-manager' ),
+				'not_found_in_trash' => __( 'No location found in Trash.', 'hbg-event-manager' )
+			);
+		
+			$args = array(
+				'labels'             => $labels,
+		        'description'        => __( 'Custom post type for event', 'hbg-event-manager' ),
+				'public'             => true,
+				'publicly_queryable' => true,
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+				'query_var'          => true,
+				'rewrite'            => array( 'slug' => __("location",'hbg-event-manager') ),
+				'capability_type'    => 'post',
+				'has_archive'        => true,
+				'hierarchical'       => false,
+				'menu_position'      => 15,
+				'supports'           => array( 'title' )
+			);
+		
+			register_post_type( 'location', $args );	
+			
+		}
+		
+		public static function register_cpt_organisation () {
+		
+			$labels = array(
+				'name'               => _x( 'Organisation', 'post type general name', 'hbg-event-manager' ),
+				'singular_name'      => _x( 'Organisation', 'post type singular name', 'hbg-event-manager' ),
+				'menu_name'          => _x( 'Organisations', 'admin menu', 'hbg-event-manager' ),
+				'name_admin_bar'     => _x( 'Organisations', 'add new on admin bar', 'hbg-event-manager' ),
+				'add_new'            => _x( 'Add New', 'book', 'hbg-event-manager' ),
+				'add_new_item'       => __( 'Add New Organisation', 'hbg-event-manager' ),
+				'new_item'           => __( 'New Organisation', 'hbg-event-manager' ),
+				'edit_item'          => __( 'Edit Organisation', 'hbg-event-manager' ),
+				'view_item'          => __( 'View Organisation', 'hbg-event-manager' ),
+				'all_items'          => __( 'All Organisations', 'hbg-event-manager' ),
+				'search_items'       => __( 'Search Organisations', 'hbg-event-manager' ),
+				'parent_item_colon'  => __( 'Parent Organisations:', 'hbg-event-manager' ),
+				'not_found'          => __( 'No organisation found.', 'hbg-event-manager' ),
+				'not_found_in_trash' => __( 'No organisation found in Trash.', 'hbg-event-manager' )
+			);
+		
+			$args = array(
+				'labels'             => $labels,
+		        'description'        => __( 'Custom post type for event-organisations', 'hbg-event-manager' ),
+				'public'             => true,
+				'publicly_queryable' => true,
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+				'query_var'          => true,
+				'rewrite'            => array( 'slug' => __("organisation",'hbg-event-manager') ),
+				'capability_type'    => 'post',
+				'has_archive'        => true,
+				'hierarchical'       => false,
+				'menu_position'      => 12,
+				'supports'           => array( 'title' )
+			);
+		
+			register_post_type( 'organisation', $args );	
 			
 		}
 		
